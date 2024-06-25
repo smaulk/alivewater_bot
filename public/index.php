c0870328423eec3e6cab72c3fe00695e25fd8f10
smaulk <st.mukhametdinov@gmail.com>
2024-06-25 21:52:32 +0500

    commit

--- public/index.php
+++ public/index.php
@@ -3,10 +3,14 @@
 use App\Controllers\MainController;
 use App\Middlewares\AuthMiddleware;
 use Pecee\SimpleRouter\SimpleRouter as Router;
+use App\Core\Env;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 Router::setDefaultNamespace('App\Controllers');
-Router::post('/', [MainController::class, 'run'])->addMiddleware(AuthMiddleware::class);
+Router::post(
+    '/'.Env::get('ROUTE'),
+    [MainController::class, 'run']
+)->addMiddleware(AuthMiddleware::class);
 Router::response()->httpCode(200);
 Router::start();
